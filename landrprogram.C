0ead696472851a4d5a096189b03f73ab8ccb82d0
harithanagaraj <36696201+harithanagaraj@users.noreply.github.com>
2018-03-28 16:20:41 +0530

    prgm

---
+++ landrprogram.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+    {
+    intj(i=0;i++;i<8);
+
+    while(n != 0)
+    {
+        // n = n/10
+        n /= 10;
+        ++count;
+    }
+
+    printf("Number of digits: %d", count);
+}
+getch();
+}
+
+
